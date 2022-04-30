2818691d2e3dde13170bb699876afcd2ea32bd21
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-30 10:36:36 +0600

    add top banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='top-banner flex justify-center items-center'>
+                <div className='text-white flex flex-col justify-center items-center w-2/4'>
+                    <h2 className='text-4xl font-bold leading-10 mb-3'>A solid network of warehouses, trucks and partners</h2>
+                    <p className='text-md font-light'>We offer our customers more benefits significant reduction in transport costs, flexible working hours and multiple direct departures for Bangladesh during the week.</p>
+                    <button className='btn px-6 py-2 border-[1px] border-white mt-6 text-xl font-medium hover:bg-[#1566AA] duration-500'>Register Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
