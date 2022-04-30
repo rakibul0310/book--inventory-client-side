8fb4cd2c67096aafb404ad115f5c7536c6ff6e6a
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-30 17:48:18 +0600

    add conditional rendering of login logout in header

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,84 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 import googleLogo from '../../images/Google-logo.png';
+import spinner from '../../images/spinner.gif';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confrimPassword, setConfrimPassword] = useState('');
+    const [errPass, setErrPass] = useState(false);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+
+    if (error || googleError) {
+        return (
+            <div className='min-h-screen text-red-500 text-center'>
+                <p>Error: {error?.message || googleError?.message}</p>
+            </div>
+        );
+    }
+    if (loading || googleLoading) {
+        return <div className=' flex justify-center items-center bg-[#F4EFEC]'>
+            <img src={spinner} alt="" />
+        </div>;
+    }
+    if (user || googleUser) {
+        return navigate(from, { replace: true });
+    }
+
+
+
+    const handleEmail = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+    }
+    const handlePassword = (event) => {
+        const value = event.target.value;
+        setPassword(value);
+    }
+    const handleConfrimPassword = (event) => {
+        const value = event.target.value;
+        setConfrimPassword(value);
+    }
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        if (password === confrimPassword) {
+            createUserWithEmailAndPassword(email, password);
+            setErrPass(false);
+        } else {
+            setErrPass(true);
+        }
+    }
     return (
         <div className='min-h-screen flex flex-col justify-center items-center'>
             <h2 className='text-lg font-medium'>Register a new account</h2>
-            <form className='flex flex-col w-3/4 md:w-1/4'>
+            <form onSubmit={handleRegister} className='flex flex-col w-3/4 md:w-1/4'>
                 <label className='text-lg mt-5 text-left' htmlFor="email">Email</label>
-                <input className='h-10 pl-3 outline-[##1566AA] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
+                <input onBlur={handleEmail} className='h-10 pl-3 outline-[#1566AA] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
                 <label className='text-lg mt-3 text-left' htmlFor="password">Password</label>
-                <input className='h-10 pl-3 outline-[##1566AA] border-[1px] border-gray-200' type="password" name="password" id="password" placeholder='Enter your password' required />
+                <input onBlur={handlePassword} className='h-10 pl-3 outline-[#1566AA] border-[1px] border-gray-200' type="password" name="password" id="password" placeholder='Enter your password' required />
                 <label className='text-lg mt-3 text-left' htmlFor="confrim-password">Confrim password</label>
-                <input className='h-10 pl-3 outline-[##1566AA] border-[1px] border-gray-200' type="password" name="password" id="confrim-password" placeholder='Confrim your password' required />
+                <input onBlur={handleConfrimPassword} className='h-10 pl-3 outline-[#1566AA] border-[1px] border-gray-200' type="password" name="password" id="confrim-password" placeholder='Confrim your password' required />
                 <input className='my-5 px-6 py-1 bg-[#1566AA] text-white text-lg font-semibold cursor-pointer' type="submit" value="Register" />
+                {
+                    errPass && <p className='text-red-500'>Password does't match</p>
+                }
                 <span className='text-sm'>Already have an account? <Link to="/login"><button className='text-blue-500 underline decoration-blue-500'>Login</button></Link></span>
             </form>
             <div className='mt-8'>
@@ -24,7 +88,7 @@ const Register = () => {
                     <div className='w-[80px] h-[3px] bg-slate-200'></div>
                 </div>
                 <div>
-                    <button className='my-5 px-6 py-1 bg-red-600 text-white hover:bg-[#1F2B6C] duration-500 text-lg font-semibold cursor-pointer rounded-sm'>
+                    <button onClick={() => signInWithGoogle()} className='my-5 px-6 py-1 bg-red-600 text-white hover:bg-[#1F2B6C] duration-500 text-lg font-semibold cursor-pointer rounded-sm'>
                         <img className='inline mr-3' src={googleLogo} alt="" height={30} width={30} />
                         Login with Google
                     </button>
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -2,10 +2,21 @@ import React, { useState } from 'react';
 import CustomLink from '../CustomLink/CustomLink.';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate()
+
+    const handleSignOut = () => {
+        signOut(auth)
+        navigate("/")
+    }
+
     return (
         <div>
             <div className='flex justify-between items-center bg-[#1566AA] py-5 px-5'>
@@ -23,7 +34,11 @@ const Header = () => {
                     <CustomLink to="/">Home</CustomLink>
                     <CustomLink to="/blogs">Blogs</CustomLink>
                     <CustomLink to="/about">About</CustomLink>
-                    <CustomLink to="/login">Login</CustomLink>
+                    {
+                        user ?
+                            <button className='btn px-6 py-1 mt-5 md:mt-0 bg-[#FDC300] text-white font-medium rounded-[50px]' onClick={handleSignOut}>Log Out</button> :
+                            <button className='btn px-6 py-1 mt-5 md:mt-0 bg-[#FDC300] text-white font-medium rounded-[50px]'><Link to="/login">Login</Link></button>
+                    }
                 </nav>
             </div >
         </div>
