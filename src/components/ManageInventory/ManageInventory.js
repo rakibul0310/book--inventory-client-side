c24fc01f77aea7d7abf4d8405f5fbaa73b960020
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-05 15:54:28 +0600

    add delete oparetion on manage inventory page

--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -8,6 +8,24 @@ const ManageInventory = () => {
             .then(res => res.json())
             .then(data => setAllItems(data))
     }, [])
+
+    const handleDeleteItem = (id) => {
+        let agree = window.confirm("Are you sure?");
+        console.log(agree);
+        if (agree) {
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const remaining = allItems.filter(item => item._id !== id)
+                    setAllItems(remaining);
+                    console.log(data);
+                })
+        }
+    }
+
     return (
         <div className='flex flex-col w-full justify-center px-5'>
             <table className='border-collapse border border-gray-300 text-center p-3 my-7'>
@@ -20,7 +38,7 @@ const ManageInventory = () => {
                 </thead>
                 <tbody>
                     {
-                        allItems.map(item => <Inventory item={item} key={item._id}></Inventory>)
+                        allItems.map(item => <Inventory item={item} handleDeleteItem={handleDeleteItem} key={item._id}></Inventory>)
                     }
                 </tbody>
             </table>
